f392f41f09723d0b1adb667f64200c27cbd6efe0
Mirko Perez <mirkoluisperez@gmail.com>
2022-03-01 16:32:37 -0300

    init app"

---
+++ src/SearchImage.js
@@ -0,0 +1,14 @@
+
+
+function SearchImage() {
+  return (
+    <div className="app">
+      <h1>Search image</h1>
+      <input name='name' type='text'/>
+
+      <hr />
+    </div>
+  );
+}
+
+export default SearchImage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SearchImage from './SearchImage';
+import './styles/styles.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SearchImage />
   </React.StrictMode>,
   document.getElementById('root')
 );
